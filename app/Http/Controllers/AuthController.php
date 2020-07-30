366e1e9c1f180f8b6d0f2c1e8530b0c379b1ed44
maadc <markus@schranz.io>
2020-07-30 15:17:43 +0200

    spotify authentication

--- app/Http/Controllers/AuthController.php
+++ app/Http/Controllers/AuthController.php
@@ -2,13 +2,24 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class AuthController extends Controller
 {
-    public function key(){
-        $key = "test";
+    public function key()
+    {
+        $url = 'https://accounts.spotify.com/api/token';
+        $data = array('client_id' => env('SPOTIFY_CLIENTID'), 'client_secret' => env('SPOTIFY_CLIENTSECRET'), 'grant_type' => 'client_credentials');
 
-        return response($key, 200);
+        $options = array(
+            'http' => array(
+                'header'  => "Content-type: application/x-www-form-urlencoded",
+                'method'  => 'POST',
+                'content' => http_build_query($data)
+            )
+        );
+        $context  = stream_context_create($options);
+        $result = file_get_contents($url, false, $context);
+        if ($result === FALSE) { return response("Authorization error ", 400);}
+
+        return response(json_decode($result)->access_token, 200);
     }
 }
