d546141bac7a337fdcba580eaf604dffa5bc1998
maadc <markus@schranz.io>
2020-07-30 14:47:44 +0200

    first version of a working API

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,5 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index');
 
-Route::get('/hallo', 'HalloController@start');
