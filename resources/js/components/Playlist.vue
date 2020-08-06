c7723307ed559fa4794d6e0fa3850f83b1c76eff
markus <markus@schranz.io>
2020-08-06 11:41:45 +0200

    option to show/hide table columns

--- resources/js/components/Playlist.vue
+++ resources/js/components/Playlist.vue
@@ -1,5 +1,34 @@
 <template>
     <div class="mt-2">
+        <div class="dropdown">
+            <a class="dropdown-toggle" href="#" tabindex="0">
+                <img src="../../img/eye.svg" title="show/hide columns" alt="show/hide columns">
+            </a>
+            <ul class="menu form-group">
+                <div class="switches" v-for="(column, index) of this.options.columns">
+                    <label class="form-switch">
+
+                        <input type="checkbox"
+                               v-bind:checked="column.visible"
+                               v-bind:id="[index]"
+                               v-on:click="hide(index, undefined)">
+                        <i class="form-icon"></i>
+                        {{ column.title }}
+                    </label>
+                    <div class="second-switches">
+                        <label class="form-switch" v-for="(columnSecond, indexSecond) of column.columns">
+                            <input type="checkbox"
+                                   v-bind:checked="columnSecond.visible"
+                                   v-bind:id="'['+ index +']'+'.'+'[' + indexSecond+ ']'"
+                                   v-on:click="hide(index, indexSecond)">
+                            <i class="form-icon"></i>
+                            {{ columnSecond.title }}
+                        </label>
+                    </div>
+                </div>
+            </ul>
+        </div>
+
         <Vue-Tabulator :options="options" v-model="playlist"/>
     </div>
 </template>
@@ -14,31 +43,37 @@ export default {
     data() {
         return {
             options: {
-                height:"calc(100vh - 1.5rem - 76px - 82px - 48px - 48px)",
-                width:"100%",
+                height: "calc(100vh - 1.5rem - 76px - 82px - 48px - 48px)",
+                width: "100%",
                 layout: "fitData",
                 frozen: true,
                 initialSort: [             //set the initial sort order of the data
                     {column: "name", dir: "asc"},
                 ],
                 columns: [
-                    {title: "Name", field: "name", width: 230},
-                    {title: "Artist", field: "artists", width: 230},
-                    {title: "Duration", field: "duration", },
-                    {title: "Popularity", field: "popularity", topCalc:"avg", },
-                    {title: "Key", field: "audioFeatures.key", topCalc:"avg", },
-                    {title: "BPM", field: "audioFeatures.tempo", topCalc:"avg", },
+                    {title: "Name", field: "name", visible: true, width: 230},
+                    {title: "Artist", field: "artists", visible: true, width: 230},
+                    {title: "Duration", field: "duration", visible: true},
+                    {title: "Popularity", field: "popularity", visible: true, topCalc: "avg"},
+                    {title: "Key", field: "audioFeatures.key", visible: true, topCalc: "avg"},
+                    {title: "BPM", field: "audioFeatures.tempo", visible: true, topCalc: "avg"},
                     {
                         title: "Audio Features",
+                        visible: true,
                         columns: [
-                            {title: "acousticness", field: "audioFeatures.acousticness", topCalc:"avg"},
-                            {title: "danceability", field: "audioFeatures.danceability", topCalc:"avg"},
-                            {title: "energy", field: "audioFeatures.energy", topCalc:"avg"},
-                            {title: "instrumentalness", field: "audioFeatures.instrumentalness", topCalc:"avg"},
-                            {title: "liveness", field: "audioFeatures.liveness", topCalc:"avg"},
-                            {title: "loudness", field: "audioFeatures.loudness", topCalc:"avg"},
-                            {title: "speechiness", field: "audioFeatures.speechiness", topCalc:"avg"},
-                            {title: "valence", field: "audioFeatures.valence", topCalc:"avg"}
+                            {title: "acousticness", field: "audioFeatures.acousticness", visible: true, topCalc: "avg"},
+                            {title: "danceability", field: "audioFeatures.danceability", visible: true, topCalc: "avg"},
+                            {title: "energy", field: "audioFeatures.energy", visible: true, topCalc: "avg"},
+                            {
+                                title: "instrumentalness",
+                                field: "audioFeatures.instrumentalness",
+                                visible: true,
+                                topCalc: "avg"
+                            },
+                            {title: "liveness", field: "audioFeatures.liveness", visible: true, topCalc: "avg"},
+                            {title: "loudness", field: "audioFeatures.loudness", visible: true, topCalc: "avg"},
+                            {title: "speechiness", field: "audioFeatures.speechiness", visible: true, topCalc: "avg"},
+                            {title: "valence", field: "audioFeatures.valence", visible: true, topCalc: "avg"}
                         ]
                     }
                 ],
@@ -48,6 +83,24 @@ export default {
     props: ['playlist'], //containing the signed fetchURL
     created() {
     },
-    methods: {}
+    methods: {
+        hide(index, indexSecond) {
+            if(indexSecond === undefined){
+
+                const checked = document.getElementById(index).checked;
+
+                if (this.options.columns[index].columns !== undefined) {
+                    this.options.columns[index].columns.forEach((ele, eleindex) => {
+                        document.getElementById('[' + index + '].[' + String(eleindex) + ']').checked = checked;
+                        ele.visible = checked;
+                    })
+                }
+                this.options.columns[index].visible = checked;
+            } else {
+                const checked = document.getElementById('['+ index +']'+'.'+'[' + indexSecond + ']').checked;
+                this.options.columns[index].columns[indexSecond].visible = checked;
+            }
+        }
+    }
 }
 </script>
--- resources/views/playlist.blade.php
+++ resources/views/playlist.blade.php
@@ -1,5 +1,9 @@
 @extends('layout.master')
 
+<!--
+todo: share button with signed URL
+-->
+
 @section('content')
     <div id="playlist-container" class="container">
             <h1 class="h3"> {{$playlist->name}}</h1>
